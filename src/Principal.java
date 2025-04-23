90f0b8ff5190e739b5edb13ffb2e53ba05609a0d
motorniko <albertolagos59@gmail.com>
2025-04-22 23:40:41 -0400

    Dando formato al codigo

--- src/Principal.java
+++ src/Principal.java
@@ -3,24 +3,28 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese un dia de la semana: ");
+
+        System.out.print("Ingrese un día de la semana: ");
         String diaSem = scanner.nextLine();
 
         diaHabil(diaSem);
     }
-    public static void diaHabil (String diaSem){
-        if (diaSem.equalsIgnoreCase("sabado") || diaSem.equalsIgnoreCase("domingo")){
-            System.out.println("el dia " + diaSem +" no es habil");
-        }
-        else if (diaSem.equalsIgnoreCase("lunes")
-                 || diaSem.equalsIgnoreCase("martes")
-                 || diaSem.equalsIgnoreCase("miercoles")
-                 || diaSem.equalsIgnoreCase("jueves")
-                 || diaSem.equalsIgnoreCase("viernes")){
-            System.out.println("El dia "+diaSem+" es habil");
-        }
-        else {
-            System.out.println("Ingrese un dia valido");
+
+    public static void diaHabil(String diaSem) {
+        if (diaSem.equalsIgnoreCase("sábado") ||
+                diaSem.equalsIgnoreCase("sabado") ||
+                diaSem.equalsIgnoreCase("domingo")) {
+            System.out.println("El día " + diaSem + " no es hábil.");
+        } else if (diaSem.equalsIgnoreCase("lunes") ||
+                diaSem.equalsIgnoreCase("martes") ||
+                diaSem.equalsIgnoreCase("miércoles") ||
+                diaSem.equalsIgnoreCase("miercoles") ||
+                diaSem.equalsIgnoreCase("jueves") ||
+                diaSem.equalsIgnoreCase("viernes")) {
+            System.out.println("El día " + diaSem + " es hábil.");
+        } else {
+            System.out.println("Por favor, ingrese un día válido.");
         }
     }
 }
+
